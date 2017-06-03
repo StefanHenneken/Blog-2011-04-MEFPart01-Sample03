fe3b0311db7b9401a9910805b7e527e27a9ec39a
Stefan Henneken <stefan.henneken@live.com>
2017-06-03 23:08:01 +0200

    Add project files.

---
+++ CarBMW/BMW.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.Composition;
+namespace CarBMW
+{
+    [Export(typeof(CarHost.ICarContract))]
+    public class BMW : CarHost.ICarContract
+    {
+        private BMW()
+        {
+            Console.WriteLine("BMW constructor");
+        }
+        [ImportingConstructor]
+        private BMW([Import("ConstructorParameter")]int parameter)
+        {
+            Console.WriteLine(String.Format("Parameter: {0}.", parameter));
+        }
+        public string StartEngine(string name)
+        {
+            return String.Format("{0} starts the BMW.", name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace CarHost
+{
+    class Program
+    {
+        [Import(typeof(ICarContract))]
+        private Lazy<ICarContract> carPart { get; set; }
+
+        [Export("ConstructorParameter")]
+        private int Parameter { get; set; }
+       
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+        void Run()
+        {          
+            var catalog = new DirectoryCatalog(".");
+            var container = new CompositionContainer(catalog);
+
+            this.Parameter = 10;
+            container.ComposeParts(this);
+            Console.WriteLine(carPart.Value.StartEngine("Sebastian"));
+            container.Dispose();
+        }
+    }
+}
